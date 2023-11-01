8690b139297d92d7658ec4d33fd708545d6028fe
Varun Gaikwad <gaikwadvarun23@gmail.com>
2023-11-01 22:43:40 +0530

    PokeDex

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import PokemonTile from './PokemonTile';
 
 function App() {
+  const pokemon_baseurl = 'https://pokeapi.co',
+    url_suffix = '/api/v2/pokemon/';
+
+  const [pokemons, setPokemons] = useState([]);
+
+  function pokemonAPI(offset, limit) {
+    fetch(`${pokemon_baseurl}${url_suffix}?offset=${offset}&limit=${limit}`).then(ele => ele.json()).then(ele => {
+      setPokemons(prev => {
+        return [...prev, ...ele.results];
+      })
+    });
+  }
+
+  useEffect(() => {
+    pokemonAPI(0, 16);
+  }, []);
+
+
+
   return (
     <div className="App">
-      <div className="MainBody">
-        <div>
-          Varundasd
+      <div className="App__Body">
+        <div className="App__Body__Header">
+          Pokémon Pokédex
+        </div>
+        <div className="PokedexBody">
+          {
+            pokemons.map((element, idx) => <PokemonTile key={idx} name={element.name} url={element.url} />)
+          }
         </div>
       </div>
     </div>
