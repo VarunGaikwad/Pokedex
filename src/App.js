7c8ce4ed711133f5a894f043ffdf7b631419c94a
VarunGaikwad <gaikwadvarun23@gmail.com>
2023-11-09 05:15:10 +0000

    New Pokedex

--- src/App.js
+++ src/App.js
@@ -1,39 +1,32 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import PokemonTile from './PokemonTile';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const pokemon_baseurl = 'https://pokeapi.co',
-    url_suffix = '/api/v2/pokemon/';
+  const pokemon_baseurl = "https://pokeapi.co",
+    url_suffix = "/api/v2/pokemon/";
 
   const [pokemons, setPokemons] = useState([]);
 
   function pokemonAPI(offset, limit) {
-    fetch(`${pokemon_baseurl}${url_suffix}?offset=${offset}&limit=${limit}`).then(ele => ele.json()).then(ele => {
-      setPokemons(prev => {
-        return [...prev, ...ele.results];
-      })
-    });
+    fetch(`${pokemon_baseurl}${url_suffix}?offset=${offset}&limit=${limit}`)
+      .then((ele) => ele.json())
+      .then((ele) => {
+        setPokemons((prev) => {
+          return [...prev, ...ele.results];
+        });
+      });
   }
 
   useEffect(() => {
     pokemonAPI(0, 16);
   }, []);
 
-
-
   return (
-    <div className="App">
-      <div className="App__Body">
-        <div className="App__Body__Header">
-          Pokémon Pokédex
-        </div>
-        <div className="PokedexBody">
-          {
-            pokemons.map((element, idx) => <PokemonTile key={idx} name={element.name} url={element.url} />)
-          }
-        </div>
-      </div>
+    <div className="app">
+      <header>
+        <h2>Pokémon Database</h2>
+      </header>
+      <main>Varun</main>
     </div>
   );
 }
